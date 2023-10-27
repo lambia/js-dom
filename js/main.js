01263a8ef6a84df3cae5e22712af8f5a8b6ce36c
Luca Lambiase <luca.lambia@gmail.com>
2023-10-27 14:16:18 +0200

    minor

--- js/main.js
+++ js/main.js
@@ -1,20 +1,18 @@
 let nomeUtente = prompt("Inserisci il tuo nome");
 let el = document.querySelector("#message");
-let btn = document.querySelector("#mioBottone")
+let btn = document.querySelector("#mioBottone");
+let input = document.querySelector("#testoUtente");
 
 // btn.addEventListener(nomeEvento, funzioneDaEseguire);
 btn.addEventListener("click", function() {
-    console.log( el.classList );
+    //Questo codice verrà eseguito solo quando l'utente cliccherà sul button
+    //E mostrerà il valore che abbiamo nell'input IN QUEL MOMENTO
+    alert("L'utente ha scritto: " + input.value);
 });
-
 
 if( nomeUtente==null || nomeUtente=="" ) {
     el.classList.add("error");
     el.innerHTML = "Utente anonimo, non sei il benvenuto";
-
-} else if (nomeUtente=="luca") {
-    el.classList.add("superuser");
-    el.innerHTML = `Benvenuto, ${nomeUtente}`;
 
 } else {
     el.classList.add("valid");
